22f8a138d5dc5e9ccebee6ff5ba6b1d1d4b408b9
Madcat888 <madcat888@163.com>
2017-10-19 01:05:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ fi
 
 #Install SSR and SSR-Bash
 cd /usr/local
-git clone https://github.com/shadowsocksr/shadowsocksr.git
+git clone https://github.com/Madcat888/shadowsocksr.git
 git clone https://github.com/${GH_REPO}.git
 cd /usr/local/shadowsocksr
 bash initcfg.sh
@@ -150,9 +150,3 @@ sed -i "s/SERVER_PUB_ADDR = '127.0.0.1'/SERVER_PUB_ADDR = '$(wget -qO- -t1 -T2 i
 #INstall Success
 bash /usr/local/SSR-Bash-Python/self-check.sh
 echo '安装完成！输入 ssr 即可使用本程序~'
-echo 'Telegram Group: https://t.me/functionclub'
-echo 'Google Puls: https://plus.google.com/communities/113154644036958487268'
-echo 'Github: https://github.com/FunctionClub'
-echo 'QQ Group:277717865'
-echo 'Function Club 无限期停更说明'
-echo 'https://www.ixh.me/2017/05/function-club-stop/'
